541ff096b0924e73aef8361ce2423cdeafde6358
anr4876 <dustjdanr@naver.com>
2022-04-09 19:32:40 +0900

    mailing_list

--- mailing_list/mailing_list/mailing_list.cpp
+++ mailing_list/mailing_list/mailing_list.cpp
@@ -11,7 +11,7 @@ typedef struct Personal_information {  // Personal_information 노드 형식
 };
 
 
-Personal_information* Add_list(Personal_information* point, Personal_information* head )  // 노드 생성하고 head에 연결
+Personal_information* Add_list(Personal_information* point, Personal_information* head)  // 노드 생성하고 head에 연결
 {
 	Personal_information data;
 	Personal_information* pd = &data;
@@ -21,28 +21,49 @@ Personal_information* Add_list(Personal_information* point, Personal_information
 	cin >> data.name;									// 이름 저장
 	cout << "번호를 입력하세요 : ";
 	cin >> data.phone_number;							// 번호 저장
-	if (point = NULL) {									// 처음 노드를 헤드에 연결
-		data.next = head;
-		return pd;										// 지금 만든 노드의 주소 반환
-	}
-	data.next = NULL;
+	data.next = point;									// 지금 생성한 노드의 다음 주소를 설정해준다
 	return pd;
 }
+
+void list_print(Personal_information* Tail) {
+	Personal_information* list = Tail;
+	
+	while(list->next == NULL) {
+		list = list->next;
+		cout << list->address << "\t";
+		cout << list->name << "\t";
+		cout << list->phone_number<<endl;
+
+
+	}
+ }
+
 
 void main() 
 {
 	Personal_information Tail;						// 꼬리 생성
 	Personal_information head;						// 머리 생성
 	Tail.next = NULL;								// 꼬리 앞의 주소 NULL지정
-	Personal_information* point = NULL;				// 전에 만든 노드의 주소를 저장할 변수
+	head.next = NULL;
+	Personal_information* point = &head;			// 전에 만든 노드의 주소를 저장할 변수
+	int i = 0;
 	while (1) {
-		Personal_information* pdata = Add_list(point, &head);		// 노드 생성후 pdata에 만든 노드의 주소 저장
-		Tail.next = pdata;											// 방금 만든 노드에 꼬리를 이어줌
-		
-		if (point != NULL) {
-			*pdata->next = *point;									// 만약 전에 만든 노드가 있으면 꼬리 -> 두번쨰에 만든 노드 -> 처음만든 노드 -> 머리 순으로 연결
+		cout << "정보를 추가하려면 1, 삭제는 2, 확인은 3을 입력하시오 : ";
+		cin >> i;
+		list_print(&Tail);
+		if (i == 1) {
+			Personal_information* pdata = Add_list(point, &head);		// 노드 생성후 pdata에 만든 노드의 주소 저장
+			Tail.next = pdata;											// 방금 만든 노드에 꼬리를 이어줌
+			point = pdata;
 		}
-		point = pdata;								// 만든 노드의 주소를 저장
+		else if (i == 2) {
+
+		}
+		else {
+
+		}
+		
+									// 만든 노드의 주소를 저장
 		
 	}
 	
